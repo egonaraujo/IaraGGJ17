4ec0c9e9e348b1b47e04feb85dae3bd7a69ed9e7
Lucas Fernandes de Oliveira <lfo14@inf.ufpr.br>
2017-01-21 15:19:43 -0200

    Add images and some scripts
    Signed-off-by: Lucas Fernandes de Oliveira <lfo14@inf.ufpr.br>

---
+++ Assets/scripts/gameMasterScript.js
@@ -0,0 +1,28 @@
+﻿#pragma strict
+
+var bar : GameObject;
+private var fishers : Array;
+var fisherPrefab : GameObject;
+private var score : int;
+
+function Start () {
+    fishers = new Array();
+}
+
+function Update () {
+	
+}
+
+function emitColor (color : int) {
+    var success : boolean = false;
+    for (var fisher : GameObject in fishers) {
+        var fisherBhv : fisherScript = fisher.GetComponent(fisherScript) as fisherScript;
+        success = success || fisherBhv.listenColor(color);
+    }
+
+    return success;
+}
+
+function addScore() {
+    ++score;
+}
