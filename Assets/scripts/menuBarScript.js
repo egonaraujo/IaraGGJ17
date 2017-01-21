c327fa7a1790f9a7ce19b559538f3d3d7851980b
Marcela Ribeiro de Oliveira <mro15@inf.ufpr.br>
2017-01-21 19:48:45 -0200

    menuBar basic structure

---
+++ Assets/scripts/menuBarScript.js
@@ -0,0 +1,46 @@
+﻿#pragma strict
+
+var cursor : GameObject;
+var bar : GameObject;
+var gameMaster : GameObject;
+
+var timePressed : float;
+var isPressioned : boolean;
+var timeWait : float;
+var deltaTime : float;
+
+var options = ["Jogar", "Melhores Colocações", "Instruções"];
+
+function Start () {
+  deltaTime = 0;
+  isPressioned = false;
+  timeWait = 2;
+}
+
+function Update () {
+  startTimer();
+  checkUp();
+}
+
+function startTimer(){
+  if(Input.GetButtonDown("Jump") && (!isPressioned)) { // Spacebar Pressed
+    timePressed = Time.time;
+    isPressioned = true;
+  }
+}
+
+function checkUp(){
+  if(Input.GetButtonUp("Jump") && (isPressioned==true)){
+    deltaTime = Time.time - timePressed;
+    Debug.Log("Precionado por: " + deltaTime);
+    Debug.Log("timeWait: " + timeWait);
+    if(deltaTime>timeWait){
+      //mudar a scene pra opção escolhida
+      isPressioned = false;
+      Debug.Log("Maior escolhe");
+    }else{
+      isPressioned = false;
+      Debug.Log("Menor muda");
+    }
+  }
+}
